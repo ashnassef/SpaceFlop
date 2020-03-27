0ec5f34d801b8e7075b7e68a00f77c10510aa289
Ash Nassef <ashnassef1@gmail.com>
2020-03-27 16:24:37 -0400

    Added ship functionality.

--- Source/SpaceFlop/Ship.cpp
+++ Source/SpaceFlop/Ship.cpp
@@ -2,8 +2,14 @@
 
 
 #include "Ship.h"
+#include "Camera/CameraComponent.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Engine.h"
+#include "GameFramework/PlayerController.h"
+
+#define PrintGreen(text, val) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(text, val))
+#define PrintRed(text, val) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(text, val))
 
 // Sets default values
 AShip::AShip()
@@ -11,11 +17,21 @@ AShip::AShip()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	m_BoxCollider = CreateDefaultSubobject<UBoxComponent>("Box Collider");
-	RootComponent = m_BoxCollider;
+	m_Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	RootComponent = m_Camera;
 
-	m_ShipMesh = CreateDefaultSubobject<UStaticMeshComponent>("Ship Mesh");
-	m_ShipMesh->SetupAttachment(RootComponent);
+	m_BoxCollider = CreateDefaultSubobject<UBoxComponent>("Box Collider");
+	m_BoxCollider->SetSimulatePhysics(true);
+	m_BoxCollider->SetEnableGravity(false);
+	m_BoxCollider->SetupAttachment(RootComponent);
+
+	m_ShipBodyMesh = CreateDefaultSubobject<UStaticMeshComponent>("Ship Body Mesh");
+    m_ShipBodyMesh->SetupAttachment(m_BoxCollider);
+
+    m_ShipGlassMesh = CreateDefaultSubobject<UStaticMeshComponent>("Ship Glass Mesh");
+    m_ShipGlassMesh->SetupAttachment(m_ShipBodyMesh);
+
+	m_ShipSpeed = 5000.0f;
 }
 
 // Called when the game starts or when spawned
@@ -30,9 +46,48 @@ void AShip::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (m_BoxCollider->GetRelativeLocation().Z > 930.0f)
+	{
+		m_BoxCollider->GetBodyInstance()->ClearForces(true);
+		m_BoxCollider->GetBodyInstance()->SetLinearVelocity(FVector::ZeroVector, false);
+		m_BoxCollider->SetRelativeLocation(FVector(m_BoxCollider->GetRelativeLocation().X, m_BoxCollider->GetRelativeLocation().Y, 930.0f));
+	}
+
+    if (m_BoxCollider->GetRelativeLocation().Z < -930.0f)
+    {
+        m_BoxCollider->GetBodyInstance()->ClearForces(true);
+        m_BoxCollider->GetBodyInstance()->SetLinearVelocity(FVector::ZeroVector, false);
+        m_BoxCollider->SetRelativeLocation(FVector(m_BoxCollider->GetRelativeLocation().X, m_BoxCollider->GetRelativeLocation().Y, -930.0f));
+    }
+}
+
+void AShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis("MoveUp", this, &AShip::MoveUp);
+}
+
+void AShip::MoveUp(float Value)
+{
+	if (Value > 0 && m_BoxCollider->GetRelativeLocation().Z < 930.0f)
+	{
+        m_BoxCollider->GetBodyInstance()->AddImpulse(FVector::UpVector * m_ShipSpeed, true);
+        PrintGreen(TEXT("Relative Z Location: %f"), m_BoxCollider->GetRelativeLocation().Z);
+	}
+	
+	if (Value < 0 && m_BoxCollider->GetRelativeLocation().Z > -930.0f)
+	{
+		m_BoxCollider->GetBodyInstance()->AddImpulse(FVector::UpVector * -m_ShipSpeed, true);
+        PrintGreen(TEXT("Relative Z Location: %f"), m_BoxCollider->GetRelativeLocation().Z);
+    }
 }
 
 void AShip::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (OtherActor->ActorHasTag("LooseObject"))
+	{
+
+	}
 }
 
